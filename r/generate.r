f9d541e3e7abdb69052eb24185f11f74bb44e535
akash-shendure <akash.shendure@gmail.com>
2022-06-13 00:11:46 -0700

    data upload

--- r/generate.r
+++ r/generate.r
@@ -3,35 +3,42 @@ raw <- read.csv("./csv/data.csv")
 # hydra
 data <- raw[raw$game == "Hydra", ]
 statistics <- list("cr", "pk", "dtb", "kills", "assits", "dtp")
-for(inner in 1:6) {
+for (inner in 1:6) {
     data <- data[order(-data[, 3 + inner], data$hash), ]
     write.csv(head(data[!duplicated(data$name), c(1, 2, 3 + inner)], 100),
         paste("./csv/hydra/", statistics[inner], ".csv", sep = ""),
-        quote = FALSE, row.names = FALSE)
+        quote = FALSE, row.names = FALSE
+    )
 }
 
 # colosseum
 data <- raw[raw$game == "Colosseum", ]
 statistics <- list("dd", "kills")
-for(inner in 1:2) {
+for (inner in 1:2) {
     data <- data[order(-data[, 3 + inner], data$hash), ]
     write.csv(head(data[!duplicated(data$name), c(1, 2, 3 + inner)], 100),
         paste("./csv/colosseum/", statistics[inner], ".csv", sep = ""),
-        quote = FALSE, row.names = FALSE)
+        quote = FALSE, row.names = FALSE
+    )
 }
 
 # other gamemodes
-games <- list("Slayer", "Eventide",
-    "Capture the Flag", "Flag Hunters", "Core Rush")
+games <- list(
+    "Slayer", "Eventide",
+    "Capture the Flag", "Flag Hunters", "Core Rush"
+)
 gamemodes <- list("slayer", "eventide", "ctf", "fh", "cr")
 statistics <- list("score", "kills", "assists", "ks", "spm")
-for(outer in 1:5) {
+for (outer in 1:5) {
     data <- raw[raw$game == games[outer], ]
-    for(inner in 1:2) {
+    for (inner in 1:5) {
         data <- data[order(-data[, 3 + inner], data$hash), ]
         write.csv(head(data[!duplicated(data$name), c(1, 2, 3 + inner)], 100),
             paste("./csv/", gamemodes[outer], "/",
-                statistics[inner], ".csv", sep = ""),
-            quote = FALSE, row.names = FALSE)
+                statistics[inner], ".csv",
+                sep = ""
+            ),
+            quote = FALSE, row.names = FALSE
+        )
     }
 }
